84286ec1cabb733c3a227ff420a2f49b11feae93
Somto Zikora <zikorasomto@gmail.com>
2020-07-24 09:19:59 +0200

    squeezenet trial

--- model.py
+++ model.py
@@ -5,4 +5,4 @@ model_trainer = ModelTraining()
 model_trainer.setModelTypeAsSqueezeNet()
 
 model_trainer.setDataDirectory(r'C:\\Users\\somto\\Documents\\python\\test-tube-classifier\\test_tubes')
-model_trainer.trainModel(num_objects=20, num_experiments=10, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=20, num_experiments=1000, enhance_data=True, batch_size=32, show_network_summary=True)
